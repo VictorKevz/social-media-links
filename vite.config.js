3e5416155813c6450d220ba83387da7438236c8a
Victor Kevz <58770966+VictorKuwandira@users.noreply.github.com>
2024-04-06 17:44:02 +0300

    Mobile responsiveness

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"social-media-links",
+  base:"/social-media-links/",
   plugins: [react()],
 })
