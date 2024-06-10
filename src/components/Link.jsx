7644d1e0838932cb164ac65bc33c617826cf40e3
Victor Kevz <58770966+VictorKuwandira@users.noreply.github.com>
2024-06-10 18:02:02 +0300

    Added Dynamic Links

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,13 +1,27 @@
-
 import React from 'react';
-import "./css/link.css"
+import "./css/link.css";
+
 function Link() {
-  const links =["GitHub","Frontend Mentor","LinkedIn","Twitter","Instagram"]
+  const links = ["GitHub", "Frontend Mentor", "LinkedIn", "Twitter", "Instagram"];
+
+  function checkURL(link) {
+    if (link === "GitHub") return "https://github.com/VictorKevz";
+    if (link === "Frontend Mentor") return "https://www.frontendmentor.io/profile/VictorKevz";
+    if (link === "LinkedIn") return "https://www.linkedin.com/in/victor-kuwandira-bb6269292/";
+    if (link === "Twitter") return "https://x.com/?lang=en";
+    if (link === "Instagram") return "https://instagram.com/";
+    return "#";
+  }
+
   return (
     <div className="links-container">
-     {links.map((link,index)=>{
-      return <a className='link' key={index}>{link}</a>
-     })}
+      {links.map((link, index) => {
+        return (
+          <a href={checkURL(link)} target='_blank' className='link' key={index}>
+            {link}
+          </a>
+        );
+      })}
     </div>
   );
 }
